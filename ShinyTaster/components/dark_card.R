fa127a3e75f43a9608c1b4c4a6ef1c145622ff39
Roxy14 <GillianM2010@yahoo.com>
2026-04-01 15:17:28 +0100

    Create dark_card.R

---
+++ ShinyTaster/components/dark_card.R
@@ -0,0 +1,13 @@
+dark_card <- function(...) {
+  div(
+    style = "
+      background:#222;
+      border:1px solid #444;
+      border-radius:8px;
+      padding:15px;
+      margin-bottom:20px;
+      color:#eee;
+    ",
+    ...
+  )
+}
